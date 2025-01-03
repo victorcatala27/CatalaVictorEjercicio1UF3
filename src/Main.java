6dc863ba80fe57f74f9b8fbdaf53ddb3f679b574
Victorcatala27 <vcabo00@gmail.com>
2025-01-03 12:50:23 +0100

    Comenzamos creando el bucle while que nos permite por ahora leer todos los valores de los títulos de películas ubicados en el fichero movies.txt. Se generan también los bloques try catch para recoger las excepciones.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        ArrayList<String> movies=new ArrayList<>();
+        File file =new File ("movies.txt"); // definimos el tipo file y creamos un objeto File
+        //ruta relativa hacia el fichero
+        try {
+            Scanner scanner= new Scanner(file);
+            System.out.println("***Títulos de las Películas***");
+            while (scanner.hasNextLine()) {
+                movies.add(scanner.nextLine());
+                String titulo= scanner.nextLine();
+                System.out.println("Valor:" + movies.getLast());
+            }
+            System.out.println("Fin lectura de datos");
+
+
+
+        } catch (Exception e) {
+            System.out.println("El fichero no existe");
+        }
     }
 }
